4b57fd995974f503c4f3be41bcd80fe25dec78df
awaltherr <anton.walther@live.com>
2023-12-13 15:25:07 +0100

    (WIP) Adding error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,31 @@
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import NoteSchemaModel from "./models/noteSchema";
 
 const app = express();
 
-app.get("/", async (request, response) => {
-  const notes = await NoteSchemaModel.find().exec();
-  response.status(200).json(notes);
+app.get("/", async (request, response, nextCallback) => {
+  try {
+    const notes = await NoteSchemaModel.find().exec();
+    response.status(200).json(notes);
+  } catch (error) {
+    nextCallback(error);
+  }
 });
+
+app.use(
+  (
+    error: unknown,
+    request: Request,
+    response: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    nextCallback: NextFunction
+  ) => {
+    console.error(error);
+    let errorMessage = "Unkown error occurred";
+    if (error instanceof Error) errorMessage = error.message;
+    response.status(500).json({ error: errorMessage });
+  }
+);
 
 export default app;
