6e0f108dd900f1b84c41fdbb68eb388f6142100b
awaltherr <anton.walther@live.com>
2023-12-13 14:43:27 +0100

    Imported schema to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import NoteSchemaModel from "./models/note";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello from backend!");
+app.get("/", async (request, response) => {
+  const notes = await NoteSchemaModel.find().exec();
+  response.status(200).json(notes);
 });
 
 export default app;
