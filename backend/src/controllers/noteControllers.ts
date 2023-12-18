31807dbab3a210530884fda593d950d6cbd63f7a
awaltherr <anton.walther@live.com>
2023-12-18 15:36:56 +0100

    Added more error handling

--- backend/src/controllers/noteControllers.ts
+++ backend/src/controllers/noteControllers.ts
@@ -1,5 +1,11 @@
 import { RequestHandler } from "express";
+import mongoose from "mongoose";
 import NoteSchemaModel from "../models/noteSchema";
+
+interface EditNoteBody {
+  noteTitle?: string;
+  noteText?: string;
+}
 
 export const fetchNotes: RequestHandler = async (request, response) => {
   try {
@@ -7,16 +13,7 @@ export const fetchNotes: RequestHandler = async (request, response) => {
     response.status(200).json(notes);
   } catch (error) {
     console.log(error);
-  }
-};
-
-export const fetchNote: RequestHandler = async (request, response) => {
-  const id = request.params.id;
-  try {
-    const note = await NoteSchemaModel.findById(id).exec();
-    response.status(200).json(note);
-  } catch (error) {
-    console.log(error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -32,5 +29,76 @@ export const addNote: RequestHandler = async (request, response) => {
     response.status(201).json(newNote);
   } catch (error) {
     console.log(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const fetchNote: RequestHandler = async (request, response) => {
+  const id = request.params.id;
+
+  try {
+    const note = await NoteSchemaModel.findById(id).exec();
+    if (!note) {
+      response.status(404).json({ error: "Note was not found" });
+    } else {
+      response.status(200).json(note);
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const editNote: RequestHandler = async (request, response) => {
+  const id = request.params.id;
+  const { noteTitle, noteText }: EditNoteBody = request.body;
+
+  try {
+    if (!noteTitle) {
+      response.status(400).json({ error: "Title cannot be empty" });
+      return;
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      response.status(400).json({ error: "Invalid id" });
+      return;
+    }
+
+    const updatedNote = await NoteSchemaModel.findByIdAndUpdate(
+      id,
+      {
+        noteTitle,
+        noteText,
+      },
+      {
+        new: true,
+      }
+    ).exec();
+
+    if (!updatedNote) {
+      response.status(404).json({ error: "Note was not found" });
+    } else {
+      response.status(200).json(updatedNote);
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteNote: RequestHandler = async (request, response) => {
+  const id = request.params.id;
+
+  try {
+    const deleteNote = await NoteSchemaModel.findByIdAndUpdate(id).exec();
+
+    if (!deleteNote) {
+      response.status(404).json({ error: "Note not found" });
+    } else {
+      response.status(200).json({ message: "Deleted note" });
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/src/routes/noteRoutes.ts
+++ backend/src/routes/noteRoutes.ts
@@ -1,4 +1,10 @@
-import { fetchNotes, fetchNote, addNote } from "../controllers/noteControllers";
+import {
+  fetchNotes,
+  fetchNote,
+  addNote,
+  editNote,
+  deleteNote,
+} from "../controllers/noteControllers";
 import express from "express";
 
 const expressRouter = express.Router();
@@ -8,5 +14,9 @@ expressRouter.get("/", fetchNotes);
 expressRouter.get("/:id", fetchNote);
 
 expressRouter.post("/", addNote);
+
+expressRouter.patch("/:id", editNote);
+
+expressRouter.patch("/:id", deleteNote);
 
 export default expressRouter;
