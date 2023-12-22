34424874830b586f5a28ea01b8a4e0585ab96c94
awaltherr <anton.walther@live.com>
2023-12-22 15:04:41 +0100

    (WIP) Implementing E2E Backend tests

--- backend/cypress/e2e/spec.cy.ts
+++ backend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,52 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:5000/api/notes");
+describe("HTTP-Requests", () => {
+  let currentNotesId: string;
+
+  beforeEach(() => {
+    cy.visit("http://localhost:5000");
+
+    cy.request("GET", "http://localhost:5000/api/notes").then((response) => {
+      const currentNotes = response.body;
+      if (currentNotes.length > 0) {
+        const randomNote = Cypress._.sample(currentNotes);
+        currentNotesId = randomNote._id;
+      }
+    });
+  });
+
+  it("deletes a note", () => {
+    expect(currentNotesId).to.not.be.undefined;
+
+    cy.request(
+      "DELETE",
+      `http://localhost:5000/api/notes/${currentNotesId}`
+    ).then((response) => {
+      console.log("Svar från servern (DELETE random note):", response);
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.property("message", "Deleted note");
+    });
+  });
+
+  it("updates a note", () => {
+    expect(currentNotesId).to.not.be.undefined;
+
+    const updatedNote = {
+      noteTitle: "Updated title by Cypress",
+      noteText: "Updated text by Cypress",
+    };
+
+    cy.request(
+      "PATCH",
+      `http://localhost:5000/api/notes/${currentNotesId}`,
+      updatedNote
+    ).then((response) => {
+      console.log("Response from PATCH-Request:", response);
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.property("_id", currentNotesId);
+      expect(response.body).to.have.property(
+        "noteTitle",
+        updatedNote.noteTitle
+      );
+      expect(response.body).to.have.property("noteText", updatedNote.noteText);
+    });
   });
 });
